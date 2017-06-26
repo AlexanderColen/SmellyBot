c9cdf69f6674bc8036721753909a4a4d1cefbe83
Alexander Colen <alexanderc@live.nl>
2017-06-26 19:49:22 +0200

    Organizing.

--- SmellyDiscordBot/SmellyBot/Properties.Designer.cs
+++ SmellyDiscordBot/SmellyBot/Properties.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SmellyDiscordBot {
+namespace SmellyDiscordBot.SmellyBot {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
